67563a975815b42a85fa54306ea73c226daab83d
sofia-iliana <sofia.iliana1311@gmail.com>
2023-10-20 10:49:37 +0300

    Readme file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # to-do-list
+
+## Description
+
+The project is an app created to keep track of your daily tasks as a to-do list. Users can add, delete and edit their tasks, as well as check the completed ones.
+
+## Technologies
+
+The project is a MERN app, built with the following technologies:
+
+- React: A JavaScript library for building user interfaces.
+- React Router: A routing library for React applications.
+- Axios: A library for making HTTP requests.
+- HTML/CSS: Markup and styling for the user interface.
+- express: For building the backend server.
+- mongoose: For interacting with the MongoDB database.
+
+## Installation
+
+1. Clone the repository.
+2. Navigate to the project directory.
+3. Run the following command to install the required dependencies:
+   npm install
--- toDoList/src/App.js
+++ toDoList/src/App.js
@@ -1,15 +1,41 @@
 import "./App.css";
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsTrash3 } from "react-icons/bs";
 import { MdOutlineModeEdit } from "react-icons/md";
-
-let nextId = 0;
+import axios from "axios";
 
 export default function App() {
   const [inputValue, setValue] = useState("");
   const [listOfTasks, setTask] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:2222/todo").then(({ data }) => {
+      setTask(data);
+    });
+  }, []);
+
+  function createTodo() {
+    axios
+      .post("http://localhost:2222/todo/create", { todo: inputValue })
+      .then(({ data }) => {
+        setTask(data);
+      });
+  }
+
+  function deleteTodo(id) {
+    axios.delete("http://localhost:2222/todo/delete/" + id).then(({ data }) => {
+      setTask(data);
+    });
+  }
+
+  function updateTodo(id) {
+    axios
+      .put("http://localhost:2222/todo/update/" + id, { todo: inputValue })
+      .then(({ data }) => {
+        setTask(data);
+      });
+  }
 
   return (
     <div className="container">
@@ -21,7 +47,7 @@ export default function App() {
         />
         <button
           onClick={() => {
-            setTask([...listOfTasks, { id: nextId++, t: inputValue }]);
+            createTodo();
             setValue("");
           }}
         >
@@ -30,40 +56,33 @@ export default function App() {
       </div>
       <div className="list">
         <ul>
-          {listOfTasks.map((task) => (
-            <li key={task.id}>
-              <input type="checkbox" className="checkbox" />
-              {task.t}
-              <button
-                className="delete"
-                onClick={() => {
-                  setTask(listOfTasks.filter((e) => e.id !== task.id));
-                }}
-              >
-                <BsTrash3 style={{ color: "#803790", fontSize: "18px" }} />
-              </button>
-              <button
-                className="edit"
-                onClick={() => {
-                  setTask(
-                    listOfTasks.map((e) => {
-                      if (e.id === task.id) {
-                        e.t = inputValue;
-                        return e;
-                      } else {
-                        return e;
-                      }
-                    })
-                  );
-                  setValue("");
-                }}
-              >
-                <MdOutlineModeEdit
-                  style={{ fontSize: "18px", color: "#3AA655" }}
-                />
-              </button>
-            </li>
-          ))}
+          {listOfTasks.map((task) => {
+            return (
+              <li key={task._id}>
+                <input type="checkbox" className="checkbox" />
+                {task.todo}
+                <button
+                  className="delete"
+                  onClick={() => {
+                    deleteTodo(task._id);
+                  }}
+                >
+                  <BsTrash3 style={{ color: "#803790", fontSize: "18px" }} />
+                </button>
+                <button
+                  className="edit"
+                  onClick={() => {
+                    updateTodo(task._id);
+                    setValue("");
+                  }}
+                >
+                  <MdOutlineModeEdit
+                    style={{ fontSize: "18px", color: "#3AA655" }}
+                  />
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
